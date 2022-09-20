b1f0bf1d10c6a4c713c52cf8232018ef8a9ddaf4
Wesley Gado <wesleygado@MacBook-Air-de-Wesley.local>
2022-09-20 09:17:50 -0300

    configurando migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmConfigService } from './database/typeorm-config';
+import { ServicosModule } from './servicos/servicos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    ServicosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
